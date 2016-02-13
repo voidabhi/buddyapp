eb08b43cfcf24b3773b0871fda5936ef3b5b49c1
Abhijeet Mohan <void.aby@gmail.com>
2016-02-13 22:33:59 +0530

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,15 @@
 
+var gulp = require('gulp'),
+    runsequence = require('run-sequence'),
+    jshint = require('gulp-jshint');
+
+
+gulp.task('default', function () {
+    runsequence('lint');
+});
+
+gulp.task('lint', function () {
+    gulp.src('**/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
